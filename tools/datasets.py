02e7af241e160bc301b2c14804f7650f662682b3
nocotan <noconoco.lib@gmail.com>
2017-07-23 16:42:34 +0900

    rw

--- tools/datasets.py
+++ tools/datasets.py
@@ -43,3 +43,24 @@ def load_letter_dp(filepath):
             data.append(tmp)
 
     return (np.array(data), np.array(target))
+
+def load_letter_uv(filepath):
+    df = pd.read_csv(filepath, header=None)
+
+    data = []
+    target = []
+    for i,v in df.iterrows():
+        tmp = []
+        f = False
+        for j in range(len(v)):
+            if j == 0:
+                if v[j] == 'U':
+                    target.append(0)
+                elif v[j] == 'V':
+                    target.append(1)
+                else:
+                    break
+            tmp.append(v[j])
+        if f:
+            data.append(tmp)
+    return (np.array(data), np.array(target))
