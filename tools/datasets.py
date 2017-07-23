eff6b2dcb529bf40877221fd243ade1a8ec3d879
nocotan <noconoco.lib@gmail.com>
2017-07-23 16:52:33 +0900

    dataset

--- tools/datasets.py
+++ tools/datasets.py
@@ -18,6 +18,22 @@ def load_australian(filepath):
 
     return (np.array(data), np.array(target))
 
+def load_letter(filepath):
+    df = pd.read_csv(filepath, header=None)
+
+    data = []
+    target = []
+    for i,v in df.iterrows():
+        tmp = []
+        for j in range(len(v)):
+            if j == 0:
+                target.append(v[j])
+                continue
+            tmp.append(v[j])
+        data.append(tmp)
+
+    return (np.array(data), np.array(target))
+
 def load_letter_dp(filepath):
     df = pd.read_csv(filepath, header=None)
 
